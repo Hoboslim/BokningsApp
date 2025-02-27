17a17b87888d194d27bf81ea7227f2281f90e1d5
Teddy Blomgren <Teddy_95@live.se>
2025-02-27 11:25:08 +0100

    fixed buttons

--- BokningsApp/MainPage.xaml.cs
+++ BokningsApp/MainPage.xaml.cs
@@ -9,7 +9,15 @@
             InitializeComponent();
         }
 
-       
+        private async void OnRegisterPageButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegisterPage());
+        }
+
+        private async void OnButtonLoggin(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new InLoggad());
+        }
     }
 
 }
