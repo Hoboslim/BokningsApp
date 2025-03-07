a6136e1e276781d01b978447fb1aae6896d33e31
Noel Burman <noelburman@hotmail.com>
2025-03-07 11:38:41 +0100

    fixat inlogg för admin och användare

--- BokningsApp/Views/MainPage.xaml.cs
+++ BokningsApp/Views/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using BokningsApp.Admin;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MongoDB.Driver;
 
 namespace BokningsApp
 {
@@ -6,6 +8,7 @@ namespace BokningsApp
     {
         int count = 0;
 
+        public Models.User Users { get; set; }
         public MainPage()
         {
             InitializeComponent();
@@ -19,12 +22,31 @@ namespace BokningsApp
         private async void OnButtonLoggin(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new InLoggad());
+
+            string email = EmailEntry.Text;
+            string password = PasswordEntry.Text;
+
+            var filter = Builders<Models.User>.Filter.Eq(x => x.Email, Users.Email);
+            var user = await Data.DB.GetUserCollection().Find(filter).FirstOrDefaultAsync();
+
+            if (user != null && user.Password == password)
+            {
+                if (user.Role == "Admin")
+                {
+                    await Navigation.PushAsync(new AdminLoggedIn());
+                }
+                else if (user.Role == "User")
+                {
+                    await Navigation.PushAsync(new InLoggad());
+                }
+            }
+
         }
 
-        private async void OnAdminPage(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new AdminLoggedIn());
-        }
+        //private async void OnAdminPage(object sender, EventArgs e)
+        //{
+        //    await Navigation.PushAsync(new AdminLoggedIn());
+        //}
     }
 
 }
