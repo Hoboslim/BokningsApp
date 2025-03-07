5739f8d680b0f6a47dace57df21ba92738933387
Teddy Blomgren <Teddy_95@live.se>
2025-03-07 09:22:54 +0100

    added mongodbdrivers and link

--- BokningsApp/Data/DB.cs
+++ BokningsApp/Data/DB.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace BokningsApp.Data
 {
     class DB
     {
+        private static MongoClient GetClient()
+        {
+            const string connectionUri = "mongodb+srv://teddyblomgren:Teddy1Plugg1@cluster0.0wmw6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+            var settings = MongoClientSettings.FromConnectionString(connectionUri);
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            var client = new MongoClient(settings);
+            return client;
+        }
     }
 }
