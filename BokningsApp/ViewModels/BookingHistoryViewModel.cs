07531d3334e9fb06585cc399f59374864c517936
Teddy Blomgren <Teddy_95@live.se>
2025-03-13 11:12:16 +0100

    Cleaned up code, added function to Booking and bookinghistory

---
+++ BokningsApp/ViewModels/BookingHistoryViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using BokningsApp.Models;
+using BokningsApp.Data;
+using MongoDB.Driver;
+
+namespace BokningsApp.ViewModels
+{
+    public class BookingHistoryViewModel
+    {
+        public ObservableCollection<Bookings> OldBookings { get; set; } = new();
+        public ObservableCollection<Rooms> BookedRooms { get; set; } = new();
+
+        public BookingHistoryViewModel() { }
+
+        public async void LoadOldBookings()
+        {
+            var oldBookings = await DB.GetBookingCollection()
+                .Find(b => b.EndTime < System.DateTime.UtcNow)
+                .ToListAsync();
+            OldBookings = new ObservableCollection<Bookings>(oldBookings);
+        }
+
+
+        public async void LoadBookedRooms()
+        {
+            var bookedRooms = await DB.GetRoomCollection()
+                .Find(r => r.Bookings.Count > 0)
+                .ToListAsync();
+            BookedRooms = new ObservableCollection<Rooms>(bookedRooms);
+        }
+
+
+
+
+    }
+}
