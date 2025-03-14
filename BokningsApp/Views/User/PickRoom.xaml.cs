9824d9468b66e12c124ae08088750cc5114abe4e
Noel Burman <noelburman@hotmail.com>
2025-03-14 13:47:48 +0100

    Fixat begränsningar för när man kan boka rum, datum och tid.

--- BokningsApp/Views/User/PickRoom.xaml.cs
+++ BokningsApp/Views/User/PickRoom.xaml.cs
@@ -24,28 +24,50 @@ public partial class PickRoom : ContentPage
 
     private async void OnBookClicked(object sender, EventArgs e)
     {
-		if (_ViewModel.SelectedRoom != null && _ViewModel.StartTime != null && _ViewModel.EndTime !=null)
+		if (_ViewModel.SelectedRoom != null && _ViewModel.StartTime != null && _ViewModel.EndTime != null)
 		{
-            DateTime selectedDate = DateTime.Parse(_ViewModel.SelectedDate);
-            DateTime startDateTime = selectedDate.Date + _ViewModel.StartTime;
-            DateTime endDateTime = selectedDate.Date + _ViewModel.EndTime;
+			DateTime selectedDate = DateTime.Parse(_ViewModel.SelectedDate);
+			DateTime startDateTime = selectedDate.Date + _ViewModel.StartTime;
+			DateTime endDateTime = selectedDate.Date + _ViewModel.EndTime;
+
 			startDateTime = DateTime.SpecifyKind(startDateTime, DateTimeKind.Unspecified);
-            endDateTime = DateTime.SpecifyKind(endDateTime, DateTimeKind.Unspecified);
+			endDateTime = DateTime.SpecifyKind(endDateTime, DateTimeKind.Unspecified);
 
-            var userIdString = await SecureStorage.GetAsync("user_id");
+			var userIdString = await SecureStorage.GetAsync("user_id");
 			var userEmail = await SecureStorage.GetAsync("user_email");
-            
 
-            if (string.IsNullOrEmpty(userIdString))
+			TimeSpan minTime = new TimeSpan(7, 0, 0);
+			TimeSpan maxTime = new TimeSpan(17, 0, 0);
+
+			if (selectedDate.Date < DateTime.Today)
+			{
+				await DisplayAlert("Felaktigt datum", "Du kan inte boka för ett datum som passerat", "OK");
+				return;
+			}
+
+			if (startDateTime.TimeOfDay < minTime || startDateTime.TimeOfDay > maxTime)
+			{
+				await DisplayAlert("Felaktig tid", "Välj en tid mellan 07:00 och 17:00", "OK");
+				return;
+			}
+
+			if (endDateTime.TimeOfDay < minTime || startDateTime.TimeOfDay > maxTime)
+			{
+				await DisplayAlert("Felaktid tid", "Välj en tid mellan 07:00 och 17:00", "OK");
+				return;
+			}
+
+
+			if (string.IsNullOrEmpty(userIdString))
 			{
 				await DisplayAlert("Error", "Ingen användare inloggad", "OK");
 				return;
 			}
 
-            var userId = new ObjectId(userIdString);
-            var roomId = _ViewModel.SelectedRoom.Id;
+			var userId = new ObjectId(userIdString);
+			var roomId = _ViewModel.SelectedRoom.Id;
 
-            var existingBooking = await Data.DB.GetBookingCollection()
+			var existingBooking = await Data.DB.GetBookingCollection()
 				.Find(b => b.RoomId == roomId &&
 				b.StartTime < endDateTime &&
 				b.EndTime > startDateTime).FirstOrDefaultAsync();
@@ -54,27 +76,31 @@ public partial class PickRoom : ContentPage
 			{
 				await DisplayAlert("Bokning fel", "Rummet är redan bokat för denna tid", "OK");
 			}
-
-			var booking = new Bookings
+			else
 			{
-				Id = ObjectId.GenerateNewId(),
-				RoomName = _ViewModel.SelectedRoom.RoomName,
-                Email = userEmail,
-                UserId = userId,
-				RoomId = _ViewModel.SelectedRoom.Id,
-				StartTime = startDateTime,
-				EndTime = endDateTime,
-				Participants = new List<ObjectId> { userId }
-			};
-            var bookingsCollection = Data.DB.GetBookingCollection();
-            await bookingsCollection.InsertOneAsync(booking);
 
-			await DisplayAlert("Bokning lyckades!", "Rummet har bokats från den valda tiden","OK");
 
-        }
+				var booking = new Bookings
+				{
+					Id = ObjectId.GenerateNewId(),
+					RoomName = _ViewModel.SelectedRoom.RoomName,
+					Email = userEmail,
+					UserId = userId,
+					RoomId = _ViewModel.SelectedRoom.Id,
+					StartTime = startDateTime,
+					EndTime = endDateTime,
+					Participants = new List<ObjectId> { userId }
+				};
+				var bookingsCollection = Data.DB.GetBookingCollection();
+				await bookingsCollection.InsertOneAsync(booking);
 
-        else
-        {
+				await DisplayAlert("Bokning lyckades!", "Rummet har bokats från den valda tiden", "OK");
+
+			}
+		}
+
+		else
+		{
 			await DisplayAlert("Felaktig inmatning", "Välj ett rum och en tid", "OK");
 		}
     }
